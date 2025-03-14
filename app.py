66f9aa1b71f39979cfa41bd01d0facb039da7f0e
kevinfacundopereyra <pereyrakevinfacundo@gmail.com>
2025-03-14 16:03:31 -0300

    test4

--- app.py
+++ app.py
@@ -41,6 +41,38 @@ def empleados(pais):
     
     return jsonify({'mensaje': 'Datos traidos exitosamente!'}, { 'empleados': empleados })
     
+@app.route('/agregarproducto/<string:nombre>/<string:descripcion>/<int:precio>/<int:stock>/', methods=['POST'])    
+def agregar_producto(nombre, descripcion, precio, stock):
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO productos (nombre, descripcion, precio, stock) VALUES (%s, %s, %s,%s)', (nombre, descripcion, precio, stock))
+    mysql.connection.commit()
+    cur.close()
+    
+    producto = [{'nombre': nombre, 'descripcion': descripcion, 'precio': precio, 'stock': stock}]
+    return jsonify(producto)
+
+@app.route('/agregarpedido/<int:id_empleado>/<string:fecha_pedido>/<int:total>', methods=['POST'])
+def agregar_pedido (id_empleado, fecha_pedido, total):
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO pedidos (id_empleado, fecha_pedido, total) VALUES (%s, %s, %s)', (id_empleado, fecha_pedido, total))
+    mysql.connection.commit()
+    cur.close()
+    
+    pedido = [{'id_empleado': id_empleado, 'fecha_pedido': fecha_pedido, 'total': total}]
+    
+    return jsonify(pedido)
+ 
+@app.route('/getproducts')
+def get_products():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM productos')
+    data = cur.fetchall()
+    cur.close()
+    products = [{'nombre': row[1], 'descripcion': row[2], 'precio': row[3], 'stock': row[4] } for row in data]
+    
+        
+    return jsonify(products)
+ 
 if (__name__) == '__main__' :
     app.run(port=3000, debug=True)
 
